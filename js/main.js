48647e8564c41f0acb95c27123acc995f066c834
semiJ <04tpal04@gmail.com>
2024-11-14 18:11:12 +0900

    portfolio

---
+++ js/main.js
@@ -0,0 +1,95 @@
+$(function(){
+    let baseline = -200;
+
+    let welcome = $("#welcome").offset().top + baseline;
+    let aboutme = $(".aboutme").offset().top + baseline;
+    let bi = $(".bi").offset().top + baseline;
+    let coding = $(".coding").offset().top + baseline;
+    let design = $(".design").offset().top + baseline;
+
+// header 영역
+$(window).on("scroll", function(){
+    let sc = $(this).scrollTop();
+    // console.log(sc);
+
+    if(sc >= 100) {
+        $("header").addClass("on");
+    } else {
+        $("header").removeClass("on");
+        $("header").stop().animate({"transition":"1s"});
+    };
+});
+
+$("header li").on("click",function(){
+    let i = $(this).index();
+    let target = $("main section").eq(i).offset().top;
+    
+    $("html, body").stop().animate({scrollTop: target}, 1200);
+    // console.log(target);
+});
+
+
+// main 영역
+// welcome ain
+    $(".ani")
+        .find(".st0") 
+        .each(function (index, path) {
+            let length = path.getTotalLength();
+            console.log(length);
+    });
+
+
+// coding
+    $(".codingArea ol li").on("click", function(){
+        i = $(this).index();
+
+        $(".codingDetail .codingDetail li").removeClass("codingative");
+        $(".codingDetail .codingDetail li").eq(i).addClass("codingative");
+    });
+
+
+    $(".codingArea ol li").on("click", function(){
+        i = $(this).index();
+
+        $(".codingArea li").removeClass("codingOrderedList");
+        $(".codingArea li").eq(i).addClass("codingOrderedList");
+    });
+
+// Design
+    gsap.registerPlugin(ScrollTrigger);
+    let upBox = document.querySelectorAll(".upBox");
+    let tl = gsap.timeline({
+        scrollTrigger : {
+            trigger: ".design",
+            pin: true,
+            scrub: 3,
+            start: "top top",
+            end: "+=400%", // 시작부분에서 400%까지 스크롤한 후 종료
+            markers: false,
+        }
+    });
+
+    tl.from(upBox, {
+        y: "400%",
+        duration:"10",
+        ease: "none",
+        stagger:"10",
+        opacity: "0"
+    });
+    tl.to(upBox, {
+        y:"0"
+    });
+
+//footer
+    $(window).on("scroll", function(){
+        let viewTop = $(this).scrollTop();
+        console.log(viewTop); // 스크롤 위치 확인
+
+        if(viewTop >= 100) {
+            $(".topBtn").addClass("ftTopbtn");
+        } else {
+            $(".topBtn").removeClass("ftTopbtn");
+        };
+    });
+
+})
