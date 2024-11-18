87af85597e7666194faec0e3f697d9dd08aa684d
semeJ <04tpal04@gmail.com>
2024-11-18 15:59:08 +0900

    포트폴리오

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function(){
     // header 영역  
-    let baseline = -800;
+    let baseline = -600;
 
     let welcome = $("#welcome").offset().top + baseline;
     let aboutme = $("#aboutme").offset().top + baseline;
@@ -33,64 +33,17 @@ $(function(){
         let target = $("#main section").eq(i).offset().top;
         
         $("html, body").stop().animate({scrollTop: target}, 1200);
-        // console.log(target);
     });
 
-    // section 슬라이드
-    window.onload = function () {
-        var elm = "#main > section";
-        $(elm).each(function (index) {
-          // 개별적으로 Wheel 이벤트 적용
-          $(this).on("mousewheel DOMMouseScroll", function (e) {
-            e.preventDefault();
-            var delta = 0;
-            if (!event) event = window.event;
-            if (event.wheelDelta) {
-              delta = event.wheelDelta / 120;
-              if (window.opera) delta = -delta;
-            } else if (event.detail) delta = -event.detail / 3;
-            var moveTop = $(window).scrollTop();
-            var elmSelecter = $(elm).eq(index);
-            // 마우스휠을 위에서 아래로
-            if (delta < 0) {
-              if ($(elmSelecter).next() != undefined) {
-                try {
-                  moveTop = $(elmSelecter).next().offset().top;
-                } catch (e) {}
-              }
-              // 마우스휠을 아래에서 위로
-            } else {
-              if ($(elmSelecter).prev() != undefined) {
-                try {
-                  moveTop = $(elmSelecter).prev().offset().top;
-                } catch (e) {}
-              }
-            }
-  
-            // 화면 이동 0.8초(800)
-            $("html,body")
-              .stop()
-              .animate(
-                {
-                  scrollTop: moveTop + "px",
-                },
-                {
-                  duration: 500,
-                  complete: function () {},
-                }
-              );
-          });
-        });
-      };
-    
+    // section
 
     //coding
 
-    $(".coding .codingtextBox ol li").on("click", function(){
+    $("#coding .codingtextBox ol li").on("click", function(){
         i = $(this).index();
 
-        $(".coding .codingtextBox li").removeClass("codingOrderedList");
-        $(".coding .codingtextBox li").eq(i).addClass("codingOrderedList");
+        $("#coding .codingtextBox li").removeClass("codingOrderedList");
+        $("#coding .codingtextBox li").eq(i).addClass("codingOrderedList");
     });
 
     gsap.registerPlugin(ScrollTrigger);
@@ -113,8 +66,9 @@ $(function(){
             duration:"10",
             ease: "none",
             stagger:"5",
-            opacity: "0"
+            opacity: "0",
         });
+        
         tl.to(upBox, {
             y: "0"
         });
@@ -130,7 +84,7 @@ $(function(){
     //footer
     $(window).on("scroll", function(){
         let viewTop = $(this).scrollTop();
-        console.log(viewTop); // 스크롤 위치 확인
+        // console.log(viewTop);
 
         if(viewTop >= 100) {
             $(".topBtn").addClass("ftTopbtn");
